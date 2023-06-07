925ad8809f865b2e12254656e2fc50fecc7f2e27
Pamitha696 <pamithabootcamp@gmail.com>
2023-06-07 23:11:25 +0530

    Create add order form

--- src/app/modules/console/console-routing.module.ts
+++ src/app/modules/console/console-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsoleComponent } from './console.component';
+import {DashboardContextComponent} from "./inner-items/dashboard-context/dashboard-context.component";
+import {ManageOrderComponent} from "./inner-items/dashboard-context/orders/manage-order/manage-order.component";
+import {AddOrderComponent} from "./inner-items/dashboard-context/orders/manage-order/add-order/add-order.component";
 
-const routes: Routes = [{ path: '', component: ConsoleComponent }];
+const routes: Routes = [{ path: '', component: ConsoleComponent,children:[
+    {path:'',redirectTo:'/console/playground/product/new',pathMatch:'full'},
+    {path:'playground',component:DashboardContextComponent,children:[
+        { path:'order',component:ManageOrderComponent,children:[
+            {path:'add',component:AddOrderComponent},
+
+          ]}
+      ]}
+
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/console/console.module.ts
+++ src/app/modules/console/console.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { ConsoleRoutingModule } from './console-routing.module';
 import { ConsoleComponent } from './console.component';
+import { DashboardContextComponent } from './inner-items/dashboard-context/dashboard-context.component';
+import { ManageOrderComponent } from './inner-items/dashboard-context/orders/manage-order/manage-order.component';
+import { AddOrderComponent } from './inner-items/dashboard-context/orders/manage-order/add-order/add-order.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
-    ConsoleComponent
+    ConsoleComponent,
+    DashboardContextComponent,
+    ManageOrderComponent,
+    AddOrderComponent
   ],
   imports: [
     CommonModule,
-    ConsoleRoutingModule
+    ConsoleRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class ConsoleModule { }
